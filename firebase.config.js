805745da86ea2847477da6560b25b83c85878451
Sergei <ssikors@upv.edu.es>
2026-02-23 21:20:49 +0100

    Configure Firebase project settings

--- firebase.config.js
+++ firebase.config.js
@@ -1,7 +1,9 @@
 window.FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyCIzFF1Jf8FFIMSVrjPkvy7gg7Y2ho0Xes',
-  authDomain: 'REPLACE_ME.firebaseapp.com',
-  projectId: 'REPLACE_ME',
-  appId: 'REPLACE_ME',
-  databaseURL: 'https://REPLACE_ME-default-rtdb.firebaseio.com'
+  apiKey: 'AIzaSyCWXSzEZUgKQHSeS24ox7i8PGWkqfEjVM4',
+  authDomain: 'colony-d11f1.firebaseapp.com',
+  projectId: 'colony-d11f1',
+  storageBucket: 'colony-d11f1.firebasestorage.app',
+  messagingSenderId: '327114538328',
+  appId: '1:327114538328:web:c82bb9db2035d68e04b92d',
+  databaseURL: 'https://colony-d11f1-default-rtdb.firebaseio.com'
 };
